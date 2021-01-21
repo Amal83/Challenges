c6e89a2d0a65903ddd7292a483e15689412aa6f4
Amal83 <amal_q@hotmail.dk>
2021-01-21 23:10:34 +0100

    challenge 201

--- Challenges200/Program.cs
+++ Challenges200/Program.cs
@@ -6,7 +6,36 @@ namespace Challenges200
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //---------Challenge 201----------------------------------------------
+            Console.WriteLine("\n**************Challenge 201****************");
+            Console.WriteLine("The function takes a string, transforms all \nbut the last four characters into `#´ and returns the new masked string.\n");
+            Console.WriteLine(Maskify("4556364607935616"));
+            Console.WriteLine(Maskify("64607935616"));
+            Console.WriteLine(Maskify("1"));
+            Console.WriteLine(Maskify(""));
+        }
+        
+        /// <summary>
+        /// Challenge 201
+        /// The function takes a string, transforms all but the last four characters into `#´ and returns the new masked string.
+        /// Examples Challenge 201:
+        /// Maskify("4556364607935616") ➞ "############5616"
+        /// Maskify("64607935616") ➞ "#######5616"
+        /// Maskify("1") ➞ "1"
+        /// Maskify("") ➞ ""
+        /// </summary>
+        /// <param name="input">A sting that shuld be masked, except the last four characters.</param>
+        /// <returns>Masked string.</returns>
+        public static String Maskify(string input)
+        {
+            return input.Length > 4 ? input.Substring(input.Length - 4).PadLeft(input.Length, '#') : input;
+
+            // example for discussion
+            //if (input.Length > 4)
+            //{
+            //    return input.Substring(input.Length - 4).PadLeft(input.Length, '#');
+            //}
+            //return input;
         }
 
     }
