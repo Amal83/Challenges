f68610c7507e3c3f15b627526f660465020251e5
Amal83 <amal_q@hotmail.dk>
2021-01-21 23:22:39 +0100

    challenges 203, 204 and 205

--- Challenges200/Program.cs
+++ Challenges200/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Challenges200
 {
     class Program
     {
+        private static int[] primes = new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
         static void Main(string[] args)
         {
             //---------Challenge 201----------------------------------------------
@@ -24,6 +26,41 @@ namespace Challenges200
             ExpressFactors(4);
             ExpressFactors(10);
             ExpressFactors(60);
+            
+            //---------Challenge 203----------------------------------------------
+            Console.WriteLine("\n**************Challenge 203****************");
+            Console.WriteLine("The function that finds a target number in a list of prime numbers. \nImplement a binary search algorithm in your function. \nThe target number will be from 2 through 97. If the target is prime then return 'yes' else return 'no'.\n");
+
+            Console.WriteLine(IsPrime(primes, 3));
+            Console.WriteLine(IsPrime(primes, 4));
+            Console.WriteLine(IsPrime(primes, 67));
+            Console.WriteLine(IsPrime(primes, 36));
+            
+            //---------Challenge 204----------------------------------------------
+            Console.WriteLine("\n**************Challenge 204****************");
+            Console.WriteLine("The function to return the amount of potatoes there are in a string.\n");
+
+            Console.WriteLine(Potatoes("potato"));
+            Console.WriteLine(Potatoes("potatopotato"));
+            Console.WriteLine(Potatoes("potatoapple"));
+            
+            //---------Challenge 205----------------------------------------------
+            Console.WriteLine("\n**************Challenge 205****************");
+            Console.WriteLine("\nEncrypting.");
+            foreach (int num in Encrypt("Hello"))
+            {
+                Console.Write(num + ", ");
+            }
+
+            Console.WriteLine("\nDecrypting.");
+            Console.WriteLine(Decrypt(new int[] { 72, 33, -73, 84, -12, -3, 13, -13, -68 }));
+
+            Console.WriteLine("\nEncrypting.");
+            foreach (int num in Encrypt("Sunshine"))
+            {
+                Console.Write(num + ", ");
+            }
+            Console.WriteLine("\n**************The END****************");
         }
         
         /// <summary>
@@ -115,6 +152,85 @@ namespace Challenges200
             }
             Console.WriteLine();
         }
+        
+        /// <summary>
+        /// Challenge 203
+        /// The method finds a target number in a list of prime numbers. Implement a binary search algorithm in your function. The target number will be from 2 through 97. If the target is prime then return "yes" else return "no".
+        /// Examples:
+        /// IsPrime(primes, 3) ➞ "yes"
+        /// IsPrime(primes, 4) ➞ "no"
+        /// IsPrime(primes, 67) ➞ "yes"
+        /// IsPrime(primes, 36) ➞ "no"
+        /// </summary>
+        /// <param name="premes">fex: int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 }</param>
+        /// <param name="input"> A integer number that shuld be checked.</param>
+        /// <returns>True or false</returns>
+        public static String IsPrime(int[] premes, int input)
+        {
+            // check if it is true
+            bool flag = premes.Contains(input);
+            if (flag) return "yes";
+            return "no";
+        }
+        
+        /// <summary>
+        /// Challenge 204.
+        /// Count the amount of potatoes there are in a string.
+        /// Examples: 
+        /// Potatoes("potato") ➞ 1
+        /// Potatoes("potatopotato") ➞ 2
+        /// Potatoes("potatoapple") ➞ 1
+        /// </summary>
+        /// <param name="text">input a text</param>
+        /// <returns>amunt of potatoes</returns>
+        public static int Potatoes(string text)
+        {
+            return Regex.Matches(text, "potato").Count;
+        }
+        
+        
+        /// <summary>
+        /// Challenge 205
+        /// Examples
+        /// Encrypt("Hello") ➞ [72, 29, 7, 0, 3]
+        /// Encrypt("Sunshine") ➞ [83, 34, -7, 5, -11, 1, 5, -9]
+        /// </summary>
+        /// <returns></returns>
+        public static List<int> Encrypt(string text)
+        {
+            // list for return
+            var result = new List<int> { };
+            // keep previews char code to  subtrac from next char code
+            var decreaser = 0;
+            foreach (char c in text)
+            {
+                result.Add((int)c - decreaser);
+                decreaser = (int)c;
+
+            }
+            return result;
+        }
+        /// <summary>
+        /// Challenge 205
+        /// Examples
+        /// Decrypt([ 72, 33, -73, 84, -12, -3, 13, -13, -68 ]) ➞ "Hi there!"
+        /// </summary>
+        /// <returns></returns>
+        public static String Decrypt(int[] input)
+        {
+            var result = "";
+            //  sum of previous number to add to the upcoming number as next char code 
+            var increaser = 0;
+
+            foreach (int n in input)
+            {
+                result = result + (char)(n + increaser);
+                increaser += n;
+            }
+            return result;
+        }
+        
+        
 
     }
 }
