b49e73fd68852f61a2bc17c0ce48111fd70e1b32
Amal83 <amal_q@hotmail.dk>
2021-01-21 23:12:09 +0100

    challenge 202

--- Challenges200/Program.cs
+++ Challenges200/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Challenges200
 {
@@ -13,6 +15,15 @@ namespace Challenges200
             Console.WriteLine(Maskify("64607935616"));
             Console.WriteLine(Maskify("1"));
             Console.WriteLine(Maskify(""));
+            
+              //---------Challenge 202----------------------------------------------
+            Console.WriteLine("\n**************Challenge 202****************");
+            Console.WriteLine("The function takes a positive integer and \nreturns a string expressing how the number can be made by multiplying powers of its prime factors..\n");
+
+            ExpressFactors(2);
+            ExpressFactors(4);
+            ExpressFactors(10);
+            ExpressFactors(60);
         }
         
         /// <summary>
@@ -36,6 +47,73 @@ namespace Challenges200
             //    return input.Substring(input.Length - 4).PadLeft(input.Length, '#');
             //}
             //return input;
+        }
+        
+        /// <summary>
+        /// Challenge 202.
+        /// The method takes a positive integer and returns a string expressing how the number can be made by multiplying powers of its prime factors.
+        /// Examples:
+        /// ExpressFactors(2) ➞ "2"
+        /// ExpressFactors(4) ➞ "2^2"
+        /// ExpressFactors(10) ➞ "2 x 5"
+        /// ExpressFactors(60) ➞ "2^2 x 3 x 5"
+        /// </summary>
+        /// <param name="input">An integer number</param>
+        public static void ExpressFactors(int input)
+        {
+            var result = new List<int>();
+
+            // find out how many time can be divided by two.
+            while (input % 2 == 0)
+            {
+                result.Add(2);
+                input /= 2;
+            }
+
+            // find out other primes.
+            int factor = 3;
+            while (factor * factor <= input)
+            {
+                if (input % factor == 0)
+                {
+                    result.Add((int)factor);
+                    input /= factor;
+                }
+                else
+                {
+                    factor += 2;
+                }
+            }
+            // If num is not 1, then whatever is left is prime.
+            if (input > 1) result.Add(input);
+
+            var keeper = 0;
+            for (int x = 0; x < result.Count(); x++)
+            {
+
+                if (result.ElementAt(x) == keeper)
+                {
+                    Console.Write("^" + result.ElementAt(x));
+                    keeper = result.ElementAt(x);
+                }
+                else if (x == 0 || (x == result.Count() - 1 && result.Count() != 2))
+                {
+                    Console.Write(result.ElementAt(x));
+                    keeper = result.ElementAt(x);
+                }
+                else if (result.Count() == 2 && result.ElementAt(x) != keeper)
+                {
+                    Console.Write("x" + result.ElementAt(x));
+                    keeper = result.ElementAt(x);
+                }
+                else
+                {
+                    Console.Write("x" + result.ElementAt(x) + "x");
+                    keeper = result.ElementAt(x);
+                }
+
+            }
+            Console.WriteLine();
         }
 
     }
