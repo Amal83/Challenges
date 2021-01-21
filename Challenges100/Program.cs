11a633929ab1d78fd8a2d753e3bef6c92bb3767c
Amal83 <amal_q@hotmail.dk>
2021-01-21 17:28:30 +0100

    first commit

---
+++ Challenges100/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Challenges100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
